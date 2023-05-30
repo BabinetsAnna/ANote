0aaf383227c4bac71bf4d6f69b05bc3e34164cec
anna babinets <113774821+BabinetsAnna@users.noreply.github.com>
2023-05-30 18:20:01 +0300

    Add files via upload

---
+++ Authform.js
@@ -0,0 +1,362 @@
+import React, { useState} from 'react';
+import { View, TextInput, TouchableOpacity, Text, StyleSheet, Alert, Image } from 'react-native';
+import * as Crypto from 'expo-crypto';
+
+
+import * as SQLite from 'expo-sqlite'
+
+
+const db = SQLite.openDatabase('db.notes');
+
+db.transaction((tx) => {
+  try {
+    tx.executeSql(
+      `CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT UNIQUE,
+        password TEXT
+      );`,
+      [],
+      (_, result) => {
+        console.log('Users table created successfully!', result);
+      }
+    );
+  } catch (error) {
+    console.log('Error creating users table:', error);
+  }
+});
+
+db.transaction((tx) => {
+  try {
+    tx.executeSql(
+      `CREATE TABLE IF NOT EXISTS notes (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT,
+        content TEXT,
+        created_at INTEGER,
+        user_id INTEGER,
+        FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE ON UPDATE CASCADE
+      );`,
+      [],
+      (_, result) => {
+        console.log('Notes table created successfully!', result);
+      }
+    );
+  } catch (error) {
+    console.log('Error creating notes table:', error);
+  }
+});
+
+db.transaction((tx) => {
+  try {
+tx.executeSql(
+  `CREATE TABLE IF NOT EXISTS image (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    note_id INTEGER,
+    image_uri TEXT,
+    FOREIGN KEY (note_id) REFERENCES notes (id) ON DELETE CASCADE ON UPDATE CASCADE
+  );`,
+  [],
+  (_, result) => {
+    console.log('Note Images table created successfully!', result);
+  }
+);
+} catch (error) {
+console.log('Error creating tables:', error);
+}
+});
+
+db.transaction((tx) => {
+  try {
+tx.executeSql(
+  `CREATE TABLE IF NOT EXISTS checkbox (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    note_id INTEGER,
+    checked INTEGER,
+    textCheckbox TEXT,
+    FOREIGN KEY (note_id) REFERENCES notes (id) ON DELETE CASCADE ON UPDATE CASCADE
+  );`,
+  [],
+  (_, result) => {
+    console.log('Note checkbox table created successfully!', result);
+  }
+);
+} catch (error) {
+console.log('Error creating tables:', error);
+}
+});
+
+const validateForm = ({ username, password, confirmPassword }, isRegistration) => {
+  const errors = [];
+
+  if (!username.trim()) {
+    errors.push(`Username cannot be empty!`);
+  }
+
+  if (!password.trim() && isRegistration) {
+    errors.push(`Pasword cannot be empty!`);
+  }
+
+  if (confirmPassword && password !== confirmPassword) {
+    errors.push('Passwords do not match!');
+  }
+
+  if (password.length < 8 && isRegistration) {
+    errors.push('The password must contain at least 8 characters!');
+  }
+
+  if (errors.length > 0) {
+    throw new Error(errors.join('\n'));
+  }
+};
+
+
+const hashPassword = async (password) => {
+  try {
+    const digest = await Crypto.digestStringAsync(
+      Crypto.CryptoDigestAlgorithm.SHA256,
+      password
+    );
+
+    return digest;
+  } catch (error) {
+    console.log('Error hashing password:', error);
+    throw error;
+  }
+};
+
+const AuthForm = ({ onSubmit, isRegistration, navigation }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+
+  const handleSubmit = async () => {
+    try {
+      validateForm({ username, password, confirmPassword }, isRegistration);
+
+      await onSubmit({ username, password });
+
+    } catch (error) {
+      Alert.alert('Error', error.message);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.imgContainer}>
+      <Image source={require('./assets/logo.png')} style={styles.image} /></View>
+      <TextInput
+        style={styles.input}
+        placeholder="Username"
+        value={username}
+        onChangeText={setUsername}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        value={password}
+        secureTextEntry
+        onChangeText={setPassword}
+      />
+      {isRegistration && (
+        <TextInput
+          style={styles.input}
+          placeholder="Confirm password"
+          value={confirmPassword}
+          secureTextEntry
+          onChangeText={setConfirmPassword}
+        />
+     ) }
+     <View style={styles.buttonContainer}>
+  <TouchableOpacity
+    style={styles.button}
+    onPress={handleSubmit}
+  >
+    <Text style={[styles.buttonText, { color: '#769CA8' }]}>
+      {isRegistration ? 'Sign Up' : 'Sign In'}
+    </Text>
+  </TouchableOpacity>
+  </View>
+    </View>
+  );
+};
+
+const AuthStorage = {
+  // функція для збереження користувача
+  async saveUser(username, password) {
+    try {
+      // виконуємо транзакцію збереження в БД
+      await db.transaction((tx) => {
+        tx.executeSql('INSERT INTO users (username, password) VALUES (?, ?)', [username, password]);
+      });
+    } catch (error) {
+      console.log('Saving user error in AuthStorage:', error);
+      throw new Error('Error saving user');
+    }
+  },
+
+  // функція для отримання користувача за ім'ям та паролем
+  async getUser(username, password) {
+    return new Promise((resolve, reject) => {
+      db.transaction((tx) => {
+        tx.executeSql(
+          'SELECT * FROM users WHERE username = ? AND password = ?',
+          [username, password],
+          (_, { rows: { _array } }) => {
+            if (_array.length > 0) {
+              resolve(_array[0]); // Повернення першого знайденого запису користувача
+            } else {
+              resolve(null);
+            }
+
+          }
+        );
+      });
+    });
+  }
+};
+
+const AuthService = {
+  // функція для авторизації користувача
+  async login({ username, password }) {
+    try {
+      const hashedPassword = await hashPassword(password);
+      
+      const user = await AuthStorage.getUser(username, hashedPassword);
+     
+     
+
+      if (!user) {
+        throw new Error('Invalid username or password!');
+      }
+      console.log(`Logged in: ${username}`);
+      return user;
+    } catch (error) {
+      console.log('Authorization error:', error);
+      throw error;
+    }
+  },
+
+  async register({ username, password, confirmPassword }) {
+    // валідація форми
+    validateForm({ username, password, confirmPassword });
+  
+    try {
+      const hashedPassword = await hashPassword(password);
+      const existingUser = await AuthStorage.getUser(username, hashedPassword);
+  
+      if (existingUser !== null) {
+        throw new Error('This username already exists! ');
+      }
+  
+      await AuthStorage.saveUser(username, hashedPassword);
+  
+      console.log(`Created new user: ${username} `);
+      return { username, hashedPassword };
+    } catch (error) {
+      console.log('Registration error:', error);
+      throw error;
+    }
+  }
+}
+
+const AuthScreen = ({ navigation }) => {
+  const [isRegistration, setIsRegistration] = useState(false);
+
+  // функція для обробки форми авторизації
+  const handleLogin = async ({ username, password }) => {
+      try {
+        
+        const user = await AuthService.login({ username, password });
+        
+        Alert.alert('Congratulations!', 'User signed in successfully.');
+        console.log(`id user: ${user.id} `);
+        navigation.navigate("ANote", { userId: user.id });
+      } catch (error) {
+        Alert.alert('Error', error.message);
+      }
+    };
+
+    const handleRegistration = async ({ username, password, confirmPassword }) => {
+      try {
+        const hashedPassword = await hashPassword(password);
+       
+
+        const existingUser = await AuthStorage.getUser(username, hashedPassword);
+        if (existingUser) {
+          throw new Error('This username already exists! ');
+        }
+        const user = await AuthService.register({ username, password, confirmPassword });
+        Alert.alert('Congratulations!', 'User created successfully.');
+        console.log(`id user: ${user.id} `);
+        navigation.navigate("ANote", { userId: user.id });
+        setIsRegistration(false);
+      } catch (error) {
+        Alert.alert('Error', error.message);
+      }
+    };
+
+return (
+  <View style={styles.screen}>
+    <AuthForm onSubmit={isRegistration ? handleRegistration : handleLogin} isRegistration={isRegistration} />
+    <View style={styles.buttonContainer}>
+  <TouchableOpacity
+    style={[styles.button]}
+    onPress={() => setIsRegistration(!isRegistration)}
+  >
+    <Text style={[styles.buttonText, { color: '#769CA8' }]}>
+      {isRegistration ? 'Sign In ' : 'Sign Up '}
+    </Text>
+  </TouchableOpacity>
+</View>
+  </View>
+);
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5E9DC',
+  },
+  container: {
+    width: '75%',
+    maxWidth: 500,
+    alignItems: 'center',
+    paddingTop: '-20%',
+  },
+  imgContainer: {
+    justifyContent: 'center',
+    margin: 40,
+  },
+  image: {
+    width: '100%',
+    height: 'auto',
+    aspectRatio: 1,
+    marginTop: '-100%',
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#827568',
+    padding: 10,
+    marginVertical: 10,
+    width: '100%',
+  },
+  buttonContainer: {
+   
+    marginTop: 10,
+    alignItems: 'center',
+  },
+  button: {
+    flexDirection: 'row',
+   
+  },
+  buttonText: {
+    fontSize: 16,
+  },
+});
+
+export default AuthScreen;
+
---
+++ screens/ShowNote.js
@@ -0,0 +1,293 @@
+import { useState, useEffect } from "react";
+import { StyleSheet, Text, View, TouchableOpacity, ScrollView, Image } from "react-native";
+import { CheckBox } from 'react-native-elements';
+import * as SQLite from "expo-sqlite";
+import Icon from 'react-native-vector-icons/FontAwesome';
+const db = SQLite.openDatabase("db.notes");
+
+export default function ShowNote({ route, navigation }) {
+  const { idNote, titleNote, textNote, idUser } = route.params;
+  const [textAlignment, setTextAlignment] = useState('left');
+  const [selectedImages, setSelectedImages] = useState([]);
+  const [checkboxes, setCheckboxes] = useState([]);
+
+  const getImage = () => {
+    if (!idNote) {
+      return;
+    }
+    db.transaction((tx) => {
+      tx.executeSql(
+        'SELECT * FROM image WHERE note_id = ?',
+        [idNote],
+        (_, { rows: { _array } }) => {
+          if (_array.length > 0) {
+            const imageUrls = _array.map((image) => image.image_uri); // Отримати тільки URL-адреси зображень
+            setSelectedImages(imageUrls);
+          } else {
+            setSelectedImages([]);
+          }
+        }
+      );
+    });
+  };
+  
+  const getCheckboxes = () => {
+    if (!idNote) {
+      return;
+    }
+    db.transaction((tx) => {
+      tx.executeSql(
+        'SELECT * FROM checkbox WHERE note_id = ?',
+        [idNote],
+        (_, { rows: { _array } }) => {
+          if (_array.length > 0) {
+            const noteCheckboxes = _array.map((checkbox) => ({
+              checked: checkbox.checked === 1,
+              text: checkbox.textCheckbox,
+            }));
+            setCheckboxes(noteCheckboxes);
+          } else {
+            setCheckboxes([]);
+          }
+        }
+      );
+    });
+  };
+  
+  useEffect(() => {
+    getImage();
+    getCheckboxes();
+  }, [idNote]);
+
+  const handleDelete = () => {
+    db.transaction((tx) => {
+      tx.executeSql(`DELETE FROM notes WHERE id = ?;`, [idNote], (tx, results) => {
+        console.log('Results', results.rowsAffected);
+        if (results.rowsAffected > 0) {
+          if (selectedImages.length > 0) {
+          delImages();
+          }
+          if (checkboxes.length > 0)
+          {
+            delCheckboxes();
+          }
+          console.log('Data deleted Successfully.... ');
+        } else {
+          console.log('Failed removing....');
+        }
+      });
+    });
+    navigation.navigate('ANote');
+  };
+
+  const delImages = () => {
+    db.transaction((tx) => {
+      tx.executeSql(`DELETE FROM image WHERE note_id = ?;`, [idNote], (tx, results) => {
+        console.log('Results', results.rowsAffected);
+        if (results.rowsAffected > 0) {
+          console.log('Images deleted Successfully.... ');
+        } else {
+          console.log('Failed removing img....');
+        }
+      });
+    });
+    navigation.navigate('ANote');
+  };
+  const delCheckboxes = () => {
+    db.transaction((tx) => {
+      tx.executeSql(`DELETE FROM checkbox WHERE note_id = ?;`, [idNote], (tx, results) => {
+        console.log('Results', results.rowsAffected);
+        if (results.rowsAffected > 0) {
+          console.log('Checkboxes deleted Successfully.... ');
+        } else {
+          console.log('Failed removing checkboxes....');
+        }
+      });
+    });
+    navigation.navigate('ANote');
+  };
+
+
+  const handleEdit = () => {
+    navigation.navigate('Form', {
+      initialValues: { title: titleNote, text: textNote },
+      idNote: idNote,
+      idUser: idUser,
+      selectedImages: selectedImages,
+      checkboxes: checkboxes,
+    });
+  };
+
+  const alignLeftFunc = () => {
+    setTextAlignment('left');
+  };
+
+  const alignCentFunc = () => {
+    setTextAlignment('center');
+  };
+
+  const alignRightFunc = () => {
+    setTextAlignment('right');
+  };
+
+  const alignJustifFunc = () => {
+    setTextAlignment('justify');
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <View style={styles.headerLeft}>
+          <TouchableOpacity style={styles.headerButton} onPress={() => navigation.goBack()}>
+            <Icon name="angle-left" size={25} color='#F5E9DC' />
+          </TouchableOpacity>
+          <Text style={styles.headerText}>ANote</Text>
+        </View>
+        <View style={styles.headerRight}>
+          <TouchableOpacity onPress={handleDelete}>
+            <Text style={styles.bText}>Delete</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={handleEdit}>
+            <Text style={styles.bText}>Edit</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+       <ScrollView>
+      <View style={styles.bAlign}>
+        <TouchableOpacity style={styles.headerButton} onPress={alignLeftFunc}>
+          <Icon name="align-left" size={25} color='#769CA8' />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.headerButton} onPress={alignCentFunc}>
+          <Icon name="align-center" size={25} color='#769CA8' />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.headerButton} onPress={alignRightFunc}>
+          <Icon name="align-right" size={25} color='#769CA8' />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.headerButton} onPress={alignJustifFunc}>
+          <Icon name="align-justify" size={25} color='#769CA8' />
+        </TouchableOpacity>
+      </View>
+ 
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>{titleNote}</Text>
+      </View>
+      <View style={styles.textContainer}>
+        <Text style={[styles.text, { textAlign: textAlignment }]}>{textNote}</Text>
+      </View>
+
+      {selectedImages.length > 0 && (
+        <ScrollView contentContainerStyle={styles.imagesContainer}>
+          {selectedImages.map((image, index) => (
+            <TouchableOpacity key={index} style={styles.imageCont}>
+              <Image source={{ uri: image }} style={styles.image} />
+            </TouchableOpacity>
+          ))}
+        </ScrollView>
+      )}
+  {checkboxes.map((checkbox, index) => (
+  <View style={styles.checkboxContainer} key={index}>
+    <CheckBox
+      checked={checkbox.checked}
+      checkedColor='#769CA8'
+      disabled
+    />
+    <Text style={styles.checkboxText}>{checkbox.text}</Text>
+  </View>
+))}
+    </ScrollView>
+    </View>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F5E9DC',
+    
+  },
+
+  header:{
+    paddingTop: 50,
+    backgroundColor: '#A89787',
+    paddingBottom: 10,
+    marginBottom: 10,
+    justifyContent: 'space-between',
+    flexDirection: 'row'
+    },
+    headerLeft:{
+      justifyContent: 'flex-start',
+      flexDirection: 'row',
+    },
+    headerRight:{
+      justifyContent: 'flex-end',
+      flexDirection: 'row',
+    },
+  headerText:{
+    paddingLeft: 20,
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#F5E9DC',
+  },
+  headerButton:{
+  paddingTop: 5,
+  paddingLeft: 20,
+  },
+bText:{
+  paddingRight: 20,
+  fontSize: 20,
+  color: '#F5E9DC',
+},
+bAlign: {
+flexDirection: 'row',
+justifyContent: 'space-around'
+},
+  title: {
+  
+    fontSize: 24,
+    textAlign: 'center',
+    padding: 20,
+    color: '#827568',
+
+  },
+ text: {
+  
+    fontSize: 20,
+    color: '#827568',
+    padding: 20,
+    marginLeft: 10,
+    marginRight: 10,
+    lineHeight: 30,
+  },
+  imagesContainer: {
+    flexGrow: 1,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  imageCont: {
+    backgroundColor: '#A89787',
+    marginBottom: '10%',
+    elevation: 8,
+    borderRadius: 10,
+    padding: 20,
+  
+  },
+ 
+  image: {
+    width: 200,
+    height: 200,
+  },
+  checkboxContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10,
+    marginLeft: 20,
+  },
+  checkboxText: {
+    flex: 1,
+    marginLeft: 10,
+    fontSize: 16,
+    color: '#827568',
+  },
+});
